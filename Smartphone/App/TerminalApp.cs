a8e69ad1ba51eb905b98a32a5cfeb3c5831f1247
hav0kinho <ruallysonfelype@gmail.com>
2023-12-18 21:29:47 -0300

    Criação da pasta App, que terá a classe TerminalApp, responsável por renderizar a aplicação no Terminal

---
+++ Smartphone/App/TerminalApp.cs
@@ -0,0 +1,26 @@
+﻿namespace Smartphone.App;
+
+public class TerminalApp
+{
+    private string NomeDev {get; set;}
+    private const string Opcoes = @"
+    1 - Listar estoque
+    2 - Adicionar Nokia
+    3 - Adicionar Iphone
+    4 - Sobre
+    5 - Sair
+    "; 
+
+    public TerminalApp(string nome)
+    {
+        this.NomeDev = nome;
+    }
+
+    public void Inicio()
+    {
+        Console.WriteLine($"Loja de Smartphones");
+        Console.WriteLine($"\n{Opcoes}\n");
+
+        Console.WriteLine($"\n\n=-=-=Desenvolvido por {NomeDev} durante DecolaTech 2024=-=-=");
+    }
+}
