16cd8d52a324758638e47d470a914967a5ab545d
hav0kinho <ruallysonfelype@gmail.com>
2023-12-18 22:28:04 -0300

    Fazendo a aplicação de terminal rodar dentro do Program.cs

--- Smartphone/Program.cs
+++ Smartphone/Program.cs
@@ -1,16 +1,10 @@
 ﻿using Smartphone.Models;
+using Smartphone.App;
 
-Console.WriteLine("Smartphone Nokia:");
-SmartphoneImp nokia = new Nokia(numero: "(81) 95534-2232", modelo: "Nokia G11", imei: "C23FGII90LLK34U", memoria: 128);
-nokia.Ligar();
-nokia.InstalarAplicativo("Duolingo");
+TerminalApp app = new TerminalApp("Ruallyson Felype");
 
-Console.WriteLine("\n");
+app.Inicializar();
 
-Console.WriteLine("Smartphone iPhone");
-SmartphoneImp iphone = new Iphone(numero: "(81) 95534-2232", modelo: "iPhone 13 Pro Max", imei: "FUOF46PYR238KUP", memoria: 128);
-iphone.ReceberLigacao();
-iphone.InstalarAplicativo("Telegram");
 
 Console.ReadLine();
 
