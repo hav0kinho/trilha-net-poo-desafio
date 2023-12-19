963f77a4707421e6dbddb1b12d474e607ebf0f39
hav0kinho <ruallysonfelype@gmail.com>
2023-12-19 03:03:02 -0300

    Criação do arquivo de testes da classe Iphone e desenvolvimento de um teste simples!

---
+++ SmartphoneTestes/IphoneTestes.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Smartphone.Models;
+
+namespace SmartphoneTestes;
+
+
+public class IphoneTestes
+{
+    private Iphone _iphone;
+
+    public IphoneTestes()
+    {
+        this._iphone = new Iphone("11111111111", "modelo", "111111111111111", 1);
+    }
+
+
+    [Fact]
+    public void DeveMudarNumeroPara8142002247()
+    {
+        this._iphone.Numero = "8142002247";
+
+        Assert.Equal("8142002247", this._iphone.Numero);
+    }
+
+
+}
